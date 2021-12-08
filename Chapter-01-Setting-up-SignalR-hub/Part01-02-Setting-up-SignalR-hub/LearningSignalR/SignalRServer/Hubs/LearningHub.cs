ea03d930bcb4512a7a4dc264cb06db3a9f5a39a9
mobiletechtracker <info@mobiletechtracker.co.uk>
2021-12-08 20:27:50 +0000

    Add code samples for chapter 1

---
+++ Chapter-01-Setting-up-SignalR-hub/Part01-02-Setting-up-SignalR-hub/LearningSignalR/SignalRServer/Hubs/LearningHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class LearningHub : Hub
+    {
+        public async Task BroadcastMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SignalR-on-.NET-6---the-complete-guide
-All code samples for book "SignalR on .NET 6 - the complete guide"
+All code samples for the book "SignalR on .NET 6 - the complete guide" by Fiodar Sazanavets
